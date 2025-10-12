fc78153eff90e3d8b593772ec05a9f1139b321e4
Ayushi <ayushi.yadav215@gmail.com>
2025-10-12 13:23:46 +0530

    Time: 4 ms (5.55%), Space: 18.7 MB (22.81%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -1,60 +1,26 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-       
+       int n = matrix.size();
+       int m = matrix[0].size();
+       vector<int> row(n, 0);
+       vector<int> column(m, 0);
+       for (int i = 0; i < n; i++){
+            for (int j = 0; j < m; j++){
+                if (matrix[i][j] == 0){
+                    row[i] = 1;
+                    column[j] = 1;
 
-        int m = matrix.size();
-        int n = matrix[0].size();
-        
-        bool firstRowZero = false;
-        bool firstColZero = false;
-
-        // Step 1: Check if first row has any zero
-        for (int j = 0; j < n; j++) {
-            if (matrix[0][j] == 0) {
-                firstRowZero = true;
-                break;
-            }
-        }
-
-        // Step 2: Check if first column has any zero
-        for (int i = 0; i < m; i++) {
-            if (matrix[i][0] == 0) {
-                firstColZero = true;
-                break;
-            }
-        }
-
-        // Step 3: Use first row/column as markers
-        for (int i = 1; i < m; i++) {
-            for (int j = 1; j < n; j++) {
-                if (matrix[i][j] == 0) {
-                    matrix[i][0] = 0;
-                    matrix[0][j] = 0;
                 }
             }
-        }
-
-        // Step 4: Set zeroes using markers
-        for (int i = 1; i < m; i++) {
-            for (int j = 1; j < n; j++) {
-                if (matrix[i][0] == 0 || matrix[0][j] == 0) {
+       }
+        for (int i = 0; i < n; i++){
+            for (int j = 0; j < m; j++){
+                if (row[i] == 1 || column[j] == 1){
                     matrix[i][j] = 0;
                 }
             }
-        }
-
-        // Step 5: Set first row and column if needed
-        if (firstRowZero) {
-            for (int j = 0; j < n; j++) {
-                matrix[0][j] = 0;
-            }
-        }
-
-        if (firstColZero) {
-            for (int i = 0; i < m; i++) {
-                matrix[i][0] = 0;
-            }
-        }
+       }
+        
     }
 };
